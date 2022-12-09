af0cb87a8e914d00607f994bf72b012000298ad4
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-12-09 18:15:06 +0400

    worked

--- src/index.js
+++ src/index.js
@@ -14,13 +14,16 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(3).fill(Array(3).fill(null)),
+            squares: [null, null, null].map(() => [null, null, null]),
             xIsNext: true,
         }
     }
 
     handleClick(i, j) {
-        const squares = {...this.state.squares};
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i][j]) {
+            return;
+        }
         squares[i][j] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -37,12 +40,15 @@ class Board extends React.Component {
         );
     }
 
-    calculateWinner(matrix) {
-
-    }
-
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        console.log('winner ',winner);
+        let status;
+        if (winner) {
+            status = 'Выиграл ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -65,6 +71,56 @@ class Board extends React.Component {
             </div>
         );
     }
+}
+
+// console.log(calculateWinner([['X','X','X'],[null,null,null],[null,null,null]]))
+
+function calculateWinner(matrix) {
+    console.log(matrix);
+    let length = matrix.length
+    console.log(length);
+    let xRowWin,
+        oRowWin,
+        xColumnWin,
+        oColumnWin;
+    let xDiagonalWin = 0;
+    let oDiagonalWin = 0;
+    let xAuxiliaryDiagonalWin = 0;
+    let oAuxiliaryDiagonalWin = 0;
+    for (let i = 0; i < length; i++) {
+        xRowWin = xColumnWin = oRowWin = oColumnWin = 0;
+        for (let j = 0; j < length; j++) {
+            if (matrix[i][j] === 'X') xRowWin++;
+            if (matrix[i][j] === 'O') oRowWin++;
+            if (matrix[j][i] === 'X') xColumnWin++;
+            if (matrix[j][i] === 'O') oColumnWin++;
+            if (i === j && matrix[i][j] === 'X') xDiagonalWin++;
+            if (i === j && matrix[i][j] === 'O') oDiagonalWin++;
+            if (j === length - i - 1 && matrix[i][j] === 'X') xAuxiliaryDiagonalWin++;
+            if (j === length - i - 1 && matrix[i][j] === 'O') oAuxiliaryDiagonalWin++;
+        }
+
+        if (
+            xRowWin === 3 ||
+            xColumnWin === 3 ||
+            xDiagonalWin === 3 ||
+            xAuxiliaryDiagonalWin === 3
+        ) {
+            console.log('X win');
+            return 'X';
+        }
+
+        if (
+            oRowWin === 3 ||
+            oColumnWin === 3 ||
+            oDiagonalWin === 3 ||
+            oAuxiliaryDiagonalWin === 3
+        ) {
+            console.log('O win');
+            return 'O';
+        }
+    }
+    return null;
 }
 
 class Game extends React.Component {
